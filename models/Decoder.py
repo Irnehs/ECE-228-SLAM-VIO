cd0161c60123e7e8d1e3b80df35b00c2ec232046
Henri Schulz <henritschulz@gmail.com>
2025-05-29 18:41:31 -0700

    added a simple decoder

--- models/Decoder.py
+++ models/Decoder.py
@@ -5,8 +5,17 @@ import torch.nn.functional as F
 
 
 class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
+    def __init__(self, input_dim=64, hidden_dim=128, output_dim=7, dropout=0.3):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, output_dim),
+        )
 
-    def forward(self, x):
-        pass
+    def forward(self, context_seq, prediction_len=None):
+        summary = context_seq[:, -1, :]  # [B, F] - Take the last time step as summary
+
+        pred = self.mlp(summary)  # [B, 7]
+        return pred.unsqueeze(1)  # [B, 1, 7]
